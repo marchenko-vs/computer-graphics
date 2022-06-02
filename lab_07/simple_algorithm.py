28104e3a57183f12ce2042e287f98dcab3442d40
marchenko-vs <marchenko_vlse@mail.ru>
2022-06-02 22:14:07 +0300

    Numpy round() func was added.

--- lab_07/simple_algorithm.py
+++ lab_07/simple_algorithm.py
@@ -1,60 +1,64 @@
+import numpy as np
+
+
 def get_bin_codes(line, left_side, right_side, bottom_side, top_side):
-    firstPoint = 0b0000
-    secondPoint = 0b0000
+    code_begin_point = 0b0000
+    code_end_point = 0b0000
 
     if line[0][0] < left_side:
-        firstPoint += 0b1000
+        code_begin_point += 0b1000
 
     if line[0][0] > right_side:
-        firstPoint += 0b0100
+        code_begin_point += 0b0100
 
     if line[0][1] > bottom_side:
-        firstPoint += 0b0010
+        code_begin_point += 0b0010
 
     if line[0][1] < top_side:
-        firstPoint += 0b0001
+        code_begin_point += 0b0001
 
     if line[1][0] < left_side:
-        secondPoint += 0b1000
+        code_end_point += 0b1000
 
     if line[1][0] > right_side:
-        secondPoint += 0b0100
+        code_end_point += 0b0100
 
     if line[1][1] > bottom_side:
-        secondPoint += 0b0010
+        code_end_point += 0b0010
 
     if line[1][1] < top_side:
-        secondPoint += 0b0001
+        code_end_point += 0b0001
 
-    return firstPoint, secondPoint
+    return code_begin_point, code_end_point
 
 
 def simple_algorithm(lines, line, left_side, right_side, bottom_side, top_side):
-    binCodes = get_bin_codes(lines[line], left_side, right_side, bottom_side, top_side)
+    bin_codes = get_bin_codes(lines[line], left_side, right_side, bottom_side, top_side)
 
-    firstPoint = binCodes[0]
-    secondPoint = binCodes[1]
-    fCoordinates = lines[line][0]
-    sCoordinates = lines[line][1]
+    code_begin_point = bin_codes[0]
+    code_end_point = bin_codes[1]
 
-    if firstPoint == 0 and secondPoint == 0:
+    if code_begin_point == 0 and code_end_point == 0:
         return lines[line]
 
-    if firstPoint & secondPoint:
+    if code_begin_point & code_end_point:
         return []
+
+    begin_coordinates = lines[line][0]
+    end_coordinates = lines[line][1]
 
     flag = 1
     i = -1
-    tan = 1e30
+    tangent = 1e30
 
-    if not firstPoint:
-        result = [fCoordinates]
-        workVar = sCoordinates
+    if not code_begin_point:
+        result = [begin_coordinates]
+        workVar = end_coordinates
         i = 1
         flag = 0
-    elif not secondPoint:
-        result = [sCoordinates]
-        workVar = fCoordinates
+    elif not code_end_point:
+        result = [end_coordinates]
+        workVar = begin_coordinates
         i = 1
         flag = 0
     else:
@@ -66,34 +70,34 @@ def simple_algorithm(lines, line, left_side, right_side, bottom_side, top_side):
 
         i += 1
 
-        if fCoordinates[0] != sCoordinates[0]:
-            tan = (sCoordinates[1] - fCoordinates[1]) / (sCoordinates[0] - fCoordinates[0])
+        if begin_coordinates[0] != end_coordinates[0]:
+            tangent = (end_coordinates[1] - begin_coordinates[1]) / (end_coordinates[0] - begin_coordinates[0])
 
             if workVar[0] <= left_side:
-                crosser = tan * (left_side - workVar[0]) + workVar[1]
+                crosser = tangent * (left_side - workVar[0]) + workVar[1]
 
                 if (crosser <= bottom_side) and (crosser >= top_side):
-                    result.append([left_side, int(round(crosser))])
+                    result.append([left_side, int(np.round(crosser))])
                     continue
             elif workVar[0] >= right_side:
-                crosser = tan * (right_side - workVar[0]) + workVar[1]
+                crosser = tangent * (right_side - workVar[0]) + workVar[1]
 
                 if (crosser <= bottom_side) and (crosser >= top_side):
-                    result.append([right_side, int(round(crosser))])
+                    result.append([right_side, int(np.round(crosser))])
                     continue
 
-        if fCoordinates[1] != sCoordinates[1]:
+        if begin_coordinates[1] != end_coordinates[1]:
             if workVar[1] <= top_side:
-                crosser = (top_side - workVar[1]) / tan + workVar[0]
+                crosser = (top_side - workVar[1]) / tangent + workVar[0]
 
                 if (crosser >= left_side) and (crosser <= right_side):
-                    result.append([int(round(crosser)), top_side])
+                    result.append([int(np.round(crosser)), top_side])
                     continue
             elif workVar[1] >= bottom_side:
-                crosser = (bottom_side - workVar[1]) / tan + workVar[0]
+                crosser = (bottom_side - workVar[1]) / tangent + workVar[0]
 
                 if (crosser >= left_side) and (crosser <= right_side):
-                    result.append([int(round(crosser)), bottom_side])
+                    result.append([int(np.round(crosser)), bottom_side])
                     continue
 
     return result
